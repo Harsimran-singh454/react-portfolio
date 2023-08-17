baf482aecbe0ef57b704dbdb32fce410736b80e8
Harsimran-singh454 <n01562454@humber.ca>
2023-08-17 15:53:39 -0400

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header'
-import Footer from './components/footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './views/Home'
